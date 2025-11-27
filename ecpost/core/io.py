56172a7067e3d2f3014e53a53fd0dcde0c2dc19b
asozza <asozza.ph@gmail.com>
2025-11-27 15:30:22 +0100

    fixing test_reader

--- ecpost/core/io.py
+++ ecpost/core/io.py
@@ -15,8 +15,8 @@ import xarray as xr
 import cftime
 import dask
 
-from outpost.utils import config
-from outpost.utils import catalogue
+from ecpost.core import config
+from ecpost.core import catalogue
 
 # dask optimization
 #dask.config.set({'array.optimize_blockwise': True})
