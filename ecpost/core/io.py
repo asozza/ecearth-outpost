1ee92cd985218cdfdbc28dd758f04cadaa7fc578
asozza <asozza.ph@gmail.com>
2025-11-28 11:12:00 +0100

    update io module

--- ecpost/core/io.py
+++ ecpost/core/io.py
@@ -67,41 +67,6 @@ def detect_axis(ds, axis_type, where='dims', verbose=False):
     return None
 
 ##########################################################################################
-# Reader of NEMO domain
-
-def preproc_nemo_domain(data):
-    """ Pre-processing routine for nemo domain """
-
-    data = data.rename({'time_counter': 'time'})
-
-    return data
-
-
-def read_domain(orca):
-    """ Read NEMO domain configuration file """
-
-    dirs = config.folders('')
-    filename = os.path.join(dirs['domain'], orca, 'domain_cfg.nc')
-    domain = xr.open_mfdataset(filename, preprocess=preproc_nemo_domain)
-    domain = domain.isel(time=0)
-
-    return domain
-
-
-def elements(orca):
-    """ Define differential forms for integrals """
-
-    df = {}
-    domain = read_domain(orca)
-    df['V'] = domain['e1t']*domain['e2t']*domain['e3t_0']
-    df['S'] = domain['e1t']*domain['e2t']
-    df['x'] = domain['e1t']
-    df['y'] = domain['e2t']
-    df['z'] = domain['e3t_0']
-
-    return df
-
-##########################################################################################
 # Readers of NEMO output
 
 def _nemodict(grid, freq):
