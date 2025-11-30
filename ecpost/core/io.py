e245cd9d3175f9b2838b8691c05c00adb4cbb637
asozza <asozza.ph@gmail.com>
2025-11-30 21:54:47 +0100

    update io

--- ecpost/core/io.py
+++ ecpost/core/io.py
@@ -10,6 +10,8 @@ Date: Nov 2025
 
 import os
 import glob
+import shutil
+import subprocess
 import logging
 import xarray as xr
 import cftime
@@ -253,36 +255,98 @@ def reader_nemo_field(expname, startyear, endyear, varname, freq="1m"):
     return data
 
 ##########################################################################################
+# Rebuilder
+
+def _get_nemo_timestep(filename):
+    """ Get timestep from a NEMO restart file """
+
+    return os.path.basename(filename).split('_')[1]
+
+def rebuilder(expname, leg):
+    """Function to rebuild NEMO restart """
+
+    dirs = config.folders(expname)
+    
+    os.makedirs(os.path.join(dirs['tmp'], str(leg).zfill(3)), exist_ok=True)
+
+    rebuild_exe = os.path.join(dirs['rebuild'], "rebuild_nemo")
+  
+    for kind in ['restart', 'restart_ice']:
+        print(' Processing ' + kind)
+        flist = glob.glob(os.path.join(dirs['restart'], str(leg).zfill(3), expname + '*_' + kind + '_????.nc'))
+        tstep = _get_nemo_timestep(flist[0])
+
+        for filename in flist:
+            destination_path = os.path.join(dirs['tmp'], str(leg).zfill(3), os.path.basename(filename))
+            try:
+                os.symlink(filename, destination_path)
+            except FileExistsError:
+                pass
+
+        rebuild_command = [rebuild_exe, "-m", os.path.join(dirs['tmp'], str(leg).zfill(3), expname + "_" + tstep + "_" + kind ), str(len(flist))]
+        try:
+            print(rebuild_command)
+            subprocess.run(rebuild_command, stderr=subprocess.PIPE, text=True, check=True)
+            for file in glob.glob('nam_rebuld_*'):
+                os.remove(file)
+        except subprocess.CalledProcessError as e:
+            error_message = e.stderr
+            print(error_message)
+
+        for filename in flist:
+            destination_path = os.path.join(dirs['tmp'], str(leg).zfill(3), os.path.basename(filename))
+            os.remove(destination_path)
+
+    # copy restart
+    tstep = _get_nemo_timestep(glob.glob(os.path.join(dirs['tmp'], str(leg).zfill(3), expname + '*_restart.nc'))[0])
+    shutil.copy(os.path.join(dirs['tmp'], str(leg).zfill(3), expname + '_' + tstep + '_restart.nc'), os.path.join(dirs['tmp'], str(leg).zfill(3), 'restart.nc'))
+    shutil.copy(os.path.join(dirs['tmp'], str(leg).zfill(3), expname + '_' + tstep + '_restart_ice.nc'), os.path.join(dirs['tmp'], str(leg).zfill(3), 'restart_ice.nc'))
+
+    # delete temporary files
+    flist = glob.glob('nam_rebuild*')
+    for file in flist:
+        os.remove(file)
+
+    return None
+
+##########################################################################################
 # Reader of NEMO restart (rebuilt)
 
 def reader_rebuilt(expname, startleg, endleg):
-    """ Read rebuilt NEMO restart files """
+    """
+    Read rebuilt NEMO restart files using a list of candidate patterns.
+
+    Parameters:
+        expname (str): Nome dell'esperimento.
+        startleg (int): Numero del leg iniziale.
+        endleg (int): Numero del leg finale.
+                                          
+    Returns:
+        xarray.Dataset: Dataset unito dai file trovati.
+    """
 
     dirs = config.folders(expname)
-    
     filelist = []
-    for leg in range(startleg,endleg+1):
-        pattern = os.path.join(dirs['tmp'], str(leg).zfill(3), expname + '*_restart.nc')
-        matching_files = glob.glob(pattern)
-        filelist.extend(matching_files)
-    logging.info(' File to be loaded %s', filelist)
-    data = xr.open_mfdataset(filelist, use_cftime=True, engine="netcdf4")
 
-    return data
+    # Default patterns
+    candidate_patterns = [
+        "{expname}*_restart.nc",
+        "restart.nc"
+    ]
 
+    for leg in range(startleg, endleg + 1):
+        for pat in candidate_patterns:            
+            pattern_str = pat.format(expname=expname)
+            full_pattern = os.path.join(dirs['tmp'], str(leg).zfill(3), pattern_str)
+            matching_files = glob.glob(full_pattern)
+            filelist.extend(matching_files)
 
-def reader_rebuilt2(expname, startleg, endleg):
-    """ Read rebuilt NEMO restart files """
+    if not filelist:
+        logging.warning("No file found")
+    else:
+        logging.info("Loading File: %s", filelist)
 
-    dirs = config.folders(expname)
-    
-    filelist = []
-    for leg in range(startleg,endleg+1):        
-        pattern = os.path.join(dirs['tmp'], str(leg).zfill(3), 'restart.nc')        
-        matching_files = glob.glob(pattern)
-        filelist.extend(matching_files)
-    logging.info(' File to be loaded %s', filelist)
-    data = xr.open_mfdataset(filelist, use_cftime=True, engine="netcdf4")
+    data = xr.open_mfdataset(filelist, use_cftime=True, engine="netcdf4") if filelist else None
 
     return data
 
